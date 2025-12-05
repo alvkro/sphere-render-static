639f1787266a5ae054063445b931cf8860b40701
Àlvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-12-04 22:48:27 -0300

    criação dos getters e do método createCircle

--- src/imagem.cpp
+++ src/imagem.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <math.h>
 #include "imagem.h"
 using namespace std;
 
@@ -37,3 +38,47 @@ void Imagem::savePPM(const string& arquivo) {
         }
     }
 }
+
+int Imagem::getXc() {
+    return w / 2;
+}
+
+int Imagem::getYc() {
+    return h / 2; 
+}
+
+// Poderia otimizar essa função futuramente
+
+void Imagem::createCircle(int tamanhoRaio) {
+    int yc = getYc();
+    int xc = getXc();
+
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            int distCirculo = pow((x - xc), 2) + pow((y - yc), 2);
+
+            if (distCirculo <= pow(tamanhoRaio, 2)) {
+                // Vermelho
+                MeasuresImg[x][y].r = 255;
+            }
+            else {
+                // Branco
+                MeasuresImg[x][y].r = 255;
+                MeasuresImg[x][y].g = 255;
+                MeasuresImg[x][y].b = 255;
+            }
+        }
+    }
+}
+
+// Função experimental
+// void Imagem::alterarGradiente() {
+//     // Teste com a cor vermelha
+//     int increaseColor = 255;
+//     for (int y = 0; y < h; y++) {
+//         for (int x = 0; x < w; x++) {
+//             MeasuresImg[x][y].r = increaseColor;
+//             increaseColor *= 0.99999999999;
+//         }
+//     }
+// }
